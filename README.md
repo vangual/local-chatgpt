935d23471845eec0a0526a4d2177644bf42ba2a5
Vangual <vangual@users.noreply.github.com>
2025-02-03 21:38:22 +0100

    Add the app and some docs

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # local-chatgpt
+
+## Description
+This is pretty much a hello-world example that allows you to spin up a fully local chat-gpt clone with minimal effort.
+
+## Requirement
+- Python 3.11+
+- [Ollama](https://ollama.com/)
+- [Supported hardware](https://github.com/ollama/ollama/blob/main/docs/gpu.md)
+  
+## Installation
+1. Download and install Ollama
+2. Clone this repo and switch into it
+3. `python3 -m venv venv`
+4. `. ./venv/bin/activate`
+5. `pip install -r requirements.txt`
+6. `chainlit run local-chatgpt.py -w`
+
+If everything worked well the last command should have told you an URL to connect to or possibly even opened you a browser to it.
+
+Have fun.
---
+++ local-chatgpt.py
@@ -0,0 +1,50 @@
+import chainlit as cl
+import ollama
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    cl.user_session.set(
+        "interaction",
+        [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            }
+        ],
+    )
+
+    msg = cl.Message(content="")
+
+    start_message = """Hello!"""
+
+    for token in start_message:
+        await msg.stream_token(token)
+
+    await msg.send()
+
+
+@cl.step(type="tool")
+async def tool(input_message):
+    interaction = cl.user_session.get("interaction")
+    interaction.append({
+        "role": "user",
+        "content": input_message
+    })
+
+    response = ollama.chat(model="deepseek-r1", messages=interaction)
+    interaction.append({
+        "role": "assistant",
+        "content": response.message.content
+    })
+    return response
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    tool_res = await tool(message.content)
+    msg = cl.Message(content="")
+    for token in tool_res.message.content:
+        await msg.stream_token(token)
+
+    await msg.send()
